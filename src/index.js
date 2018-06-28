cf9cdc3a475fc5622c928f6843863948c60d164d
masoudso <veisi.masoud@gmail.com>
2018-06-28 18:29:43 -0400

    react redux package is installed and increment now works using redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux'
+import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
 import reducer from './store/reducer'
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore();
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer);
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,6 +3,11 @@ const intialState = {
 }
 
 const reducer = (state = intialState, action) => {
+    if (action.type === 'INCREMENT') {
+        return {
+            counter: state.counter + 1
+        }
+    }
     return state;
 }
 
