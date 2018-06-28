cf591445265cc229a2207b5a1bb48be622b6cda0
masoudso <veisi.masoud@gmail.com>
2018-06-28 11:44:36 -0400

    initialized redux in this app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux'
 import './index.css';
 import App from './App';
+import reducer from './store/reducer'
 import registerServiceWorker from './registerServiceWorker';
 
+const store = createStore();
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
