f8f41817141ce1a4a79ccb1cd619392094567778
masoudso <veisi.masoud@gmail.com>
2018-06-30 10:53:06 -0400

    storing and deleting counter

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -14,9 +14,16 @@ class Counter extends Component {
             <div>
                 <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
-                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
-                <CounterControl label="Add 10" clicked={this.props.onAddCounter}  />
-                <CounterControl label="Subtract 15" clicked={this.props.onSubtractCounter}  />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter} />
+                <CounterControl label="Add 10" clicked={this.props.onAddCounter} />
+                <CounterControl label="Subtract 15" clicked={this.props.onSubtractCounter} />
+                <h1 />
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {this.props.storedResult.map(sr => (
+                        <li key={sr.id} onClick={() => this.props.onDeleteResult(sr.id)}>{sr.val}</li>
+                    ))}
+                </ul>
             </div>
         );
     }
@@ -24,7 +31,8 @@ class Counter extends Component {
 
 const mapStateToProp = state => {
     return {
-        ctr : state.counter
+        ctr : state.counter,
+        storedResult: state.result
     }
 }
 
@@ -33,7 +41,9 @@ const mapDispatchToProps = dispatch => {
         onIncrementCounter: () => dispatch({type : 'INCREMENT'}),
         onDecrementCounter: () => dispatch({type : 'DECREMENT'}),
         onAddCounter: () => dispatch({type : 'ADD', val: 10 }),
-        onSubtractCounter: () => dispatch({type : 'SUB', val: 15})
+        onSubtractCounter: () => dispatch({type : 'SUB', val: 15}),
+        onStoreResult: () => dispatch({type: 'STORE_RESULT'}),
+        onDeleteResult: (id) => dispatch({type: 'DELETE_RESULT', resultId: id})
     }
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,24 +1,43 @@
 const intialState = {
-    counter : 0
+    counter : 0,
+    result : []
 }
 
 const reducer = (state = intialState, action) => {
     switch(action.type){
         case 'INCREMENT':
         return {
+            ...state,
             counter: state.counter + 1
         }
         case 'DECREMENT':
         return {
+            ...state,
             counter: state.counter - 1
         }
         case 'ADD':
         return{
+            ...state,
             counter: state.counter + action.val
         }
         case 'SUB':
         return{
+            ...state,
             counter: state.counter - action.val
+        }
+        case 'STORE_RESULT':
+        return {
+            ...state,
+            result: state.result.concat({id: new Date(), val: state.counter})
+        }
+        case 'DELETE_RESULT':
+        //by using filter function, the result (here rs) and index are being taken
+        //then the index is compared to id, if it meets the condition, filter
+        //function returns true
+        const updatedArray = state.result.filter(rs => rs.id !== action.resultId)
+        return {
+            ...state,
+            result : updatedArray
         }
     }
     return state;
